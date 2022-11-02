c87d96825637a13b0b9764d660bf2af425c7f4fb
joeunhye <masimel33@gmail.com>
2022-11-02 23:32:56 +0900

    swiper slider add

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -3,6 +3,16 @@ import React, { useEffect, useState } from "react";
 import "./Row.css";
 import MovieModal from "./MovieModal";
 
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 const Row = ({ title, id, fetchUrl, isLargeRow }) => {
 	const [movies, setMovies] = useState([]);
 	const [modalOpen, setModalOpen] = useState(false);
@@ -24,42 +34,41 @@ const Row = ({ title, id, fetchUrl, isLargeRow }) => {
 	return (
 		<section className="row">
 			<h2>{title}</h2>
-			<div className="slider">
-				<div className="slider__arrow-left">
-					<span
-						className="arrow"
-						onClick={() => {
-							document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-						}}
-					>
-						{"<"}
-					</span>
-				</div>
 
+			<Swiper
+				// install Swiper modules
+				modules={[Navigation, Pagination, Scrollbar, A11y]}
+				loop={true}
+				breakpoints={{
+					1378: {
+						slidesPerView: 6, // 한번에 보이는 슬라이드 개수
+						slidesPerGroup: 6, // 몇개씩 슬라이드 할지
+					},
+					998: {
+						slidesPerView: 5,
+						slidesPerGroup: 5,
+					},
+					625: {
+						slidesPerView: 4,
+						slidesPerGroup: 4,
+					},
+					0: {
+						slidesPerView: 3,
+						slidesPerGroup: 3,
+					},
+				}}
+				navigation
+				pagination={{ clickable: true }}
+				onSlideChange={() => console.log("slide change")}
+			>
 				<div id={id} className="row__posters">
 					{movies.map(movie => (
-						<img
-							key={movie.id}
-							className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-							src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-							alt={movie.title}
-							onClick={() => handleClick(movie)}
-						/>
+						<SwiperSlide key={movie.id}>
+							<img className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.title} onClick={() => handleClick(movie)} />
+						</SwiperSlide>
 					))}
 				</div>
-
-				<div className="slider__arrow-right">
-					<span
-						className="arrow"
-						onClick={() => {
-							document.getElementById(id).scrollLeft += window.innerWidth - 80;
-						}}
-					>
-						{">"}
-					</span>
-				</div>
-			</div>
-
+			</Swiper>
 			{modalOpen && <MovieModal {...movieSelected} setModalOpen={setModalOpen} />}
 		</section>
 	);
